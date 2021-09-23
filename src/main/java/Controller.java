5989203342a61e946138ccbe0f2c0ffbff1ba7d7
iyage <iyaghe.ofunrein@decagon.dev>
2021-09-24 00:02:15 +0100

    change maven.yaml file to accomodate heroku deploy

---
+++ src/main/java/Controller.java
@@ -0,0 +1,9 @@
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/page1")
+    public  String showPage1(){
+        return "page1";
+    }
+}
